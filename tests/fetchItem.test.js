7cd4c84e4218d884bb81090db03a2e913ef9998e
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-27 20:43:50 -0300

    feito requisito 9 - testes para a função "fetchItem"

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -2,7 +2,25 @@ require('../mocks/fetchSimulator');
 const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
-describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('2 - Teste a função fetchItem', () => {
+  it('Verifica se é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  
+  it('Verifica se chama a função "fetch"', async () => {
+    await fetchItem('MLB1615760527');
+    const urlCalled = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toHaveBeenCalled();
+    expect(fetch).toHaveBeenCalledWith(urlCalled);
+  });
+
+  it('Verifica se retorna o objeto esperado', async () => {
+    const response = await fetchItem('MLB1615760527');
+    expect(response).toEqual(item);
+  });
+
+  it('Verifica se não passar nenhum argumento, retorna um erro com a mensagem correta', async () => {
+    const response = await fetchItem();
+    expect(response).toEqual(new Error('You must provide an url'));
+  })
 });
