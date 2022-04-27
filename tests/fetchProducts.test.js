3cb0660af63c75275e8c17f2c04af38bd01f7e02
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-27 20:16:47 -0300

    feito requisito 8 - testes para a função "fetchProducts"

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,7 +2,26 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
-describe('1 - Teste a função fecthProducts', () => {
+describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se é uma função', () => {
+    expect( typeof fetchProducts ).toBe('function');
+  });
+
+  it('Verifica se ao executar, a função "fetch" é executada', async () => {
+    await fetchProducts('computador');
+    const endPointCalled = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalled();
+    expect(fetch).toHaveBeenCalledWith(endPointCalled);
+  });
+
+  it('Verifica se retorna a estrutura de dados corretamente', async () => {
+    const response = await fetchProducts('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+
+  it('Verifica se não passar nenhum argumento, retorna um erro com a mensagem correta', async ()=> {
+    const response = await fetchProducts();
+    expect(response).toEqual(new Error('You must provide an url'));
+  });
 });
