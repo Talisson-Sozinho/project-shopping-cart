13985b0710ea874497f50a39871c09584302dea6
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-28 00:40:23 -0300

    feito requisito 01 parte 1 - função "fetchProducts" implementada

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (product) => {
+  const endPointUrl = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  try {
+    const response = await fetch(endPointUrl);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
